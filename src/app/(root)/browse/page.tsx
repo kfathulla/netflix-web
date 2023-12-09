2daa864a129a994697028e00476a24167d004a48
kfathulla <kfathulla03@gmail.com>
2023-12-09 14:40:14 +0500

    Create global context

---
+++ src/app/(root)/browse/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Login from '@/components/shared/login';
+import { useGlobalContext } from '@/context';
+import React from 'react';
+
+const Page = () => {
+    const {account} = useGlobalContext();
+
+    if (account == null)
+        return <Login />
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Page;
